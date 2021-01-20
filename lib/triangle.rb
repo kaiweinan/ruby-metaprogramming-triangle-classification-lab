ea11598ac4777e76a2cc520923da5cf501718bf4
kaiweinan <kaiweinan@yahoo.com>
2021-01-20 11:48:01 -0500

    commit tree ddbb5523c60d8ddb3569b1c514af0c80c2943176

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+
+  attr_accessor :sidea, :sideb, :sidec
+
+  def initialize(sidea, sideb, sidec)
+    @sidea = sidea
+    @sideb = sideb
+    @sidec = sidec
+  end
+
+  def kind
+    validate_triangle
+    if sidea == sideb && sideb == sidec
+      :equilateral
+    elsif sidea == sideb || sideb == sidec || sidec == sidea
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def validate_triangle
+    valid_triangles = [(sidea + sideb > sidec), (sidea + sidec >sideb), (sideb + sidec > sidea)]
+    [sidea, sideb, sidec].each do |side|
+      valid_triangles << false unless side >0 
+      raise TriangleError if valid_triangles.include?(false)
+    end
+  end
+
+
+
+  class TriangleError < StandardError
+  def message
+    puts "no triangle"
+  end
+
+end
 end
